878ed485a37f4ae269a6995c7e789064dbba4806
sjijon <41587146+sjijon@users.noreply.github.com>
2023-11-13 13:01:42 +0100

    export one hist at a time

--- Figs_Appendix/FigS6_Error/plot_error_byN.R
+++ Figs_Appendix/FigS6_Error/plot_error_byN.R
@@ -16,6 +16,9 @@ allestims <- read.csv("results/All_Estims.csv")
 # Error without taking the absolute value
 allestims$DiffDelay <- allestims$ObsDaysInf1CaseN - allestims$MinTime
 
+# Sample size
+table(allestims$Dataset, allestims$FirstCases)
+
 # Values of N
 Ns <- sort(unique(allestims$FirstCases))
 
@@ -35,20 +38,23 @@ t_col <- function(colors, opacity) {
 # Define breaks for histograms (so that they look the same)
 brks <- seq(min(allestims$DiffDelay)-0.5, max(allestims$DiffDelay) + 0.5, by = 1)
 
-# Plot the result
-par(las = 1, mgp = c(2, 0.5, 0), tck = -0.025)
-layout(matrix(1:3, ncol = 1))
-for(i in 1:3){
+# Plot histograms of the results
+for(i in seq_along(Ns)){
+  pdf(paste0("results/distribution_N_", Ns[i], ".pdf"), width = 6, height = 3)
+  par(las = 1, 
+      mai = c(0.75, 0.75, 0.5, 0.2), mgp = c(1.5, 0.5, 0), tck = -0.025)
   # Subset of the data for this N
   tmp <- allestims[allestims$FirstCases == Ns[i], ]
   # Plot histogram
   hist(tmp$DiffDelay, breaks = brks, 
        col = t_col(colsN[i], 0.5), border = "white", lwd = 0.1,
-       xlab = "Difference from real date of first infection", 
-       main = paste0("N = ", Ns[i]), axes = FALSE)
+       xlab = "Difference from real date of first infection (days)", 
+       main = paste0("N = ", Ns[i]), axes = FALSE, ylim = c(0, 850))
   # All line for median
   abline(v = median(tmp$DiffDelay), col = colsN[i], lwd = 2)
+  abline(v = mean(tmp$DiffDelay), col = colsN[i], lwd = 2, lty = 2)
   # Add axes
   axis(1, pos = 0)
   axis(2, pos = brks[1])
+  dev.off()
 }
